60914bd5ae182b9d32a05a55d6f5b068b59160be
Sam Passarelli <passarelli.sam@gmail.com>
2020-10-08 02:41:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,13 +3,13 @@ require "pry"
 def game_hash
   hasketball = {
     :home => {
-      :team_name => ""
-      :colors => []
+      :team_name => "",
+      :colors => [],
       :players => []
-    }
+    },
     :away => {
-      :team_name => ""
-      :colors => []
+      :team_name => "",
+      :colors => [],
       :players => []
     }
   }
