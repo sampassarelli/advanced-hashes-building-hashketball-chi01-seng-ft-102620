0eb0a047b279cf2d1fa87535166663c31d1bc8e5
Sam Passarelli <passarelli.sam@gmail.com>
2020-10-08 03:17:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,8 +3,8 @@ require "pry"
 def game_hash
   hasketball = {
     :home => {
-      :team_name => "",
-      :colors => [],
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
       :players => [
         {:player_name => "Alan Anderson",
         :number => 0,
@@ -54,8 +54,8 @@ def game_hash
         ]
     },
     :away => {
-      :team_name => "",
-      :colors => [],
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
       :players => [
         {:player_name => "Jeff Adrien",
         :number => 0,
