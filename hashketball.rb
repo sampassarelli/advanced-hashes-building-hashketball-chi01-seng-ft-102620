d466b0018a9497f161249db069fb9af8763956ac
Sam Passarelli <passarelli.sam@gmail.com>
2020-10-08 02:53:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,16 @@ def game_hash
     :home => {
       :team_name => "",
       :colors => [],
-      :players => []
+      :players => [
+        {:player_name => ""},
+        {:number =>},
+        {:shoe =>},
+        {:points =>},
+        {:rebounds => },
+        {:assists =>},
+        {:steals =>},
+        {
+        ]
     },
     :away => {
       :team_name => "",
