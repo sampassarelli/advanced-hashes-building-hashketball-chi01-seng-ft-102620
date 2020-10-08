e0cf5894dcfc497d01f9ee32b3620c82783a958b
Sam Passarelli <passarelli.sam@gmail.com>
2020-10-08 03:15:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -57,7 +57,7 @@ def game_hash
       :team_name => "",
       :colors => [],
       :players => [
-        {:player_name => "Alan Anderson",
+        {:player_name => "Jeff Adrien",
         :number => 0,
         :shoe => 16,
         :points => 22,
