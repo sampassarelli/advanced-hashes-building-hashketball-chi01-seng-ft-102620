dbc3370f8cc4adeeba5bbd3a1c18681aebd77b84
Sam Passarelli <passarelli.sam@gmail.com>
2020-10-08 03:22:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -81,6 +81,7 @@ def game_hash
         :points => 24,
         :rebounds => 12,
         :assists => 4,
+        :steals => 4,
         :blocks => 5,
         :slam_dunks => 5},
         {:player_name => "Ben Gordon",
